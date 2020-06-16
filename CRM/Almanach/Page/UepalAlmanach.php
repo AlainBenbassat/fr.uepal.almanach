28a5bb6618e31ae3f335df26f16d7f719aef2de6
uepalog <uepalog@ssh01.cluster028.gra.hosting.ovh.net>
2020-06-16 17:47:33 +0000

    ajouts

--- CRM/Almanach/Page/UepalAlmanach.php
+++ CRM/Almanach/Page/UepalAlmanach.php
@@ -6,17 +6,106 @@ class CRM_Almanach_Page_UepalAlmanach extends CRM_Core_Page {
     CRM_Utils_System::setTitle(E::ts("Union des Églises protestantes d'Alsace et de Lorraine"));
 
     // assign template variables
-    $this->assign('epcaalTable', $this->getUpcaalTable());
+    $this->assign('epcaalTable', $this->getEpcaalTable());
+    $this->assign('epralTable', $this->getEpralTable());
 
     parent::run();
   }
 
-  private function getUpcaalTable() {
-    $t = [
-      ['aa', 'aaa', 'aaaa'],
-      ['bb', 'bbb', 'bbbb'],
-      ['cc', 'ccc', 'cccc'],
-    ];
+  private function getEpcaalTable() {
+    $t = [];
+
+    $sql = "
+      select 
+        distinct c.id, c.organization_name 
+      from 
+        civicrm_contact c 
+      inner join 
+        civicrm_value_paroisse_detail d on c.id = d.inspection_consistoire_reforme
+      where 
+        d.eglise = 2 
+      and
+        c.is_deleted = 0
+      order by 
+        c.organization_name
+    ";
+    $daoInspec = CRM_Core_DAO::executeQuery($sql);
+    while ($daoInspec->fetch()) {
+      $t[] = [$daoInspec->organization_name, '', ''];
+	    $sql = "
+	      select 
+		distinct c.id, c.organization_name 
+	      from 
+		civicrm_contact c 
+	      inner join 
+		civicrm_value_paroisse_detail d on c.id = d.consistoire_lutherien
+	      where 
+		d.inspection_consistoire_reforme = %1
+	      and
+		c.is_deleted = 0
+	      order by 
+		c.organization_name
+	    ";
+            $sqlParams = [
+              1 => [$daoInspec->id, 'Integer']
+            ];
+	    $daoConsist = CRM_Core_DAO::executeQuery($sql, $sqlParams);
+	    while ($daoConsist->fetch()) {
+                 $t[] = ['', $daoConsist->organization_name, ''];
+            }
+    }
+
+/*
+select * from civicrm_value_paroisse_detail limit 0,2;
++-----+-----------+--------+-----------+--------------------------------+-----------------------+--------------------+------------------+
+| id  | entity_id | eglise | theologie | inspection_consistoire_reforme | consistoire_lutherien
+*/
+
+    return $t;
+  }
+
+  private function getEpralTable() {
+    $t = [];
+
+    $sql = "
+      select 
+        distinct c.id, c.organization_name 
+      from 
+        civicrm_contact c 
+      inner join 
+        civicrm_value_paroisse_detail d on c.id = d.inspection_consistoire_reforme
+      where 
+        d.eglise = 1 
+      and
+        c.is_deleted = 0
+      order by 
+        c.organization_name
+    ";
+    $daoInspec = CRM_Core_DAO::executeQuery($sql);
+    while ($daoInspec->fetch()) {
+      $t[] = [$daoInspec->organization_name, ''];
+	    $sql = "
+	      select 
+		distinct c.id, c.organization_name 
+	      from 
+		civicrm_contact c 
+	      inner join 
+		civicrm_value_paroisse_detail d on c.id = d.entity_id
+	      where 
+		d.inspection_consistoire_reforme = %1
+	      and
+		c.is_deleted = 0
+	      order by 
+		c.organization_name
+	    ";
+            $sqlParams = [
+              1 => [$daoInspec->id, 'Integer']
+            ];
+	    $daoPar = CRM_Core_DAO::executeQuery($sql, $sqlParams);
+	    while ($daoPar->fetch()) {
+                 $t[] = ['', $daoPar->organization_name];
+            }
+    }
 
     return $t;
   }
--- templates/CRM/Almanach/Page/UepalAlmanach.tpl
+++ templates/CRM/Almanach/Page/UepalAlmanach.tpl
@@ -21,4 +21,23 @@
 
 <h2>Église protestante réformée d'Alsace et de Lorraine (EPRAL)</h2>
 
+<table class="report-layout display">
+  <thead>
+  <tr>
+    <th>Consistoire</th>
+    <th>Paroisses</th>
+  </tr>
+  </thead>
+  <tbody>
+  {foreach from=$epralTable item=row}
+    <tr class="{cycle values="odd-row,even-row"}">
+      <td>{$row[0]}</td>
+      <td>{$row[1]}</td>
+    </tr>
+  {/foreach}
+  </tbody>
+</table>
+<p>&nbsp;</p>
+
+
 
